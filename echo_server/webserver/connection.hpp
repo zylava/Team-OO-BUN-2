89e0afcc2f2d837df531074a1cbe665cc33dbcd0
stan <stansjiang@gmail.com>
2017-01-23 00:17:20 -0800

    changes

---
+++ echo_server/webserver/connection.hpp
@@ -0,0 +1,40 @@
+#ifndef ECHO_SERVER_H
+#define ECHO_SERVER_H
+
+#include <array>
+#include <memory>
+#include <boost/asio.hpp>
+#include <string>
+
+namespace http {
+namespace server {
+
+class connection
+  : public std::enable_shared_from_this<connection>
+{
+public:
+  connection(const connection&) = delete;
+  connection& operator=(const connection&) = delete;
+
+  void start();
+
+  void stop();
+
+private:
+  void do_read();
+
+  void do_write();
+
+  boost::asio::ip::tcp::socket socket_;
+
+  std::array<char, 8192> buffer_;
+
+  std::string reply_;
+};
+
+typedef std::shared_ptr<connection> connection_ptr;
+
+} // namespace server
+} // namespace http
+
+#endif // HTTP_CONNECTION_HPP
