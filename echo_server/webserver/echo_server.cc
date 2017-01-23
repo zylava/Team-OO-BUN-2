9816a17eaf6bb5d2d75e409a3ba34e87a77b1699
Rishub Kumar <rishub@gmail.com>
2017-01-23 06:19:24 +0000

    removed test stuff

--- echo_server/webserver/echo_server.cc
+++ echo_server/webserver/echo_server.cc
@@ -2,23 +2,4 @@
 
 int main {
 	
-	cout << "Hello World!" << endl;
-	cout << "Kevin was here" << endl; 
-	cout << "Stan" << endl;
-	cout << "Rishub" << endl;
-
-}
-
-int sum {
-	int x = 1;
-	int y = 1;
-	return x + y;
-}
-
-int subtract {
-	cout << "A - b is over 9000" << endl;
-}
-
-float multiply (float a, float b) {
-	return a * b; 
 }
