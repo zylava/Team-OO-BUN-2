970ce068022b5a87cec143aac8b62de1791b26df
stan <stansjiang@gmail.com>
2017-01-30 23:35:17 -0800

    boost::asio::async_write -> boost::asio::write, no longer prints out the response header along with the body

--- echo_server/webserver/connection.cpp
+++ echo_server/webserver/connection.cpp
@@ -33,35 +33,20 @@ void connection::do_read_and_write()
         	reply_.append(buffer_.data(), bytes_transferred);
           if (reply_.substr(reply_.size() - 4, 4) == "\r\n\r\n")
           {
-            // HTTP OK response
-            const std::string okay_reponse = "HTTP/1.1 200 OK\r\n";
-            // Content type string
-            const std::string content_type = "Content-Type: text/plain\r\n";
+            // HTTP response and content type
+            // Send back a 200 (OK) code and plain text type
+            const char* reponse_string = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
 
-            // We will be using 'response' to hold the input, a vector of type
-            // const_buffer since async_write requires a type buffer 
-            std::vector<boost::asio::const_buffer> response;
+            // We will be using 'response' to hold the request data from the user
+            std::string response;
+            response.append(reply_);
 
-            response.push_back(boost::asio::buffer(okay_reponse));
-            response.push_back(boost::asio::buffer(content_type));
-            response.push_back(boost::asio::buffer(reply_));
-
-            // Send the responses
-            for(unsigned int i = 0; i < response.size(); i++)
-            {
-              socket_.send(boost::asio::buffer(response[i])); 
-            }
-            // boost::asio:async_write requites a const ConstBufferSequence as the second argument
-            boost::asio::async_write(socket_, response,
-                [this, self](boost::system::error_code ec, std::size_t)
-                {
-                  if (!ec) {
-                    // Initiate graceful connection closure.
-                    boost::system::error_code ignored_ec;
-                    socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
-                      ignored_ec);
-                  }
-                });
+            // Cast the string into a char* as boost::asio::write requires a char*
+            // and the length of what char* points to (we use strlen for this)
+            const char* reply_body = response.c_str();
+            
+            boost::asio::write(socket_, boost::asio::buffer(reponse_string, strlen(reponse_string)));
+            boost::asio::write(socket_, boost::asio::buffer(reply_body, strlen(reply_body)));
           }
           else
           {
